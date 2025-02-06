e077b1fed45efc9022f1a832ef7364f94bcb9349
Piyush Raj <protolabzpiyush@gmail.com>
2025-02-06 18:40:24 +0530

    Add Notes and ColorPicker Controller

---
+++ Controllers/NotesController.cs
@@ -0,0 +1,17 @@
+﻿using Microsoft.AspNetCore.Mvc;
+
+namespace TaskMonitoringApp.Controllers
+{
+    public class NotesController : Controller
+    {
+        public IActionResult Index()
+        {
+            return View();
+        }
+
+        public IActionResult Create()
+        {
+            return View();
+        }
+    }
+}
