d68168a215a148319f77437ea3611564cd6db6ba
Piyush Raj <protolabzpiyush@gmail.com>
2025-02-06 12:26:43 +0530

    Add the unique name by userId in both task and goal models

---
+++ Migrations/20250206060444_AddUniqueConstrainOnTaskAndGoalModels.cs
@@ -0,0 +1,90 @@
+﻿using Microsoft.EntityFrameworkCore.Migrations;
+
+#nullable disable
+
+namespace TaskMonitoringApp.Migrations
+{
+    /// <inheritdoc />
+    public partial class AddUniqueConstrainOnTaskAndGoalModels : Migration
+    {
+        /// <inheritdoc />
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.DropIndex(
+                name: "IX_Tasks_UserId",
+                table: "Tasks");
+
+            migrationBuilder.DropIndex(
+                name: "IX_Goals_UserId",
+                table: "Goals");
+
+            migrationBuilder.AlterColumn<string>(
+                name: "Name",
+                table: "Tasks",
+                type: "nvarchar(450)",
+                nullable: false,
+                defaultValue: "",
+                oldClrType: typeof(string),
+                oldType: "nvarchar(max)",
+                oldNullable: true);
+
+            migrationBuilder.AlterColumn<string>(
+                name: "Name",
+                table: "Goals",
+                type: "nvarchar(450)",
+                nullable: false,
+                oldClrType: typeof(string),
+                oldType: "nvarchar(max)");
+
+            migrationBuilder.CreateIndex(
+                name: "IX_Tasks_UserId_Name",
+                table: "Tasks",
+                columns: new[] { "UserId", "Name" },
+                unique: true);
+
+            migrationBuilder.CreateIndex(
+                name: "IX_Goals_UserId_Name",
+                table: "Goals",
+                columns: new[] { "UserId", "Name" },
+                unique: true);
+        }
+
+        /// <inheritdoc />
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.DropIndex(
+                name: "IX_Tasks_UserId_Name",
+                table: "Tasks");
+
+            migrationBuilder.DropIndex(
+                name: "IX_Goals_UserId_Name",
+                table: "Goals");
+
+            migrationBuilder.AlterColumn<string>(
+                name: "Name",
+                table: "Tasks",
+                type: "nvarchar(max)",
+                nullable: true,
+                oldClrType: typeof(string),
+                oldType: "nvarchar(450)");
+
+            migrationBuilder.AlterColumn<string>(
+                name: "Name",
+                table: "Goals",
+                type: "nvarchar(max)",
+                nullable: false,
+                oldClrType: typeof(string),
+                oldType: "nvarchar(450)");
+
+            migrationBuilder.CreateIndex(
+                name: "IX_Tasks_UserId",
+                table: "Tasks",
+                column: "UserId");
+
+            migrationBuilder.CreateIndex(
+                name: "IX_Goals_UserId",
+                table: "Goals",
+                column: "UserId");
+        }
+    }
+}
--- Models/Entities/Tasks.cs
+++ Models/Entities/Tasks.cs
@@ -34,7 +34,7 @@ namespace TaskMonitoringApp.Models.Entities
     {
         public int Id { get; set; }
 
-        public string? Name { get; set; }
+        public string Name { get; set; }
 
         public string? Description { get; set; }
 
