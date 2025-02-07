dfcd4fcb3a65d590f0ad9860a85dd71853bb629f
Piyush Raj <protolabzpiyush@gmail.com>
2025-02-07 15:33:36 +0530

    Add Db_migration update Notes model fields

---
+++ Migrations/20250207073442_Update NotesModle.cs
@@ -0,0 +1,40 @@
+﻿using System;
+using Microsoft.EntityFrameworkCore.Migrations;
+
+#nullable disable
+
+namespace TaskMonitoringApp.Migrations
+{
+    /// <inheritdoc />
+    public partial class UpdateNotesModle : Migration
+    {
+        /// <inheritdoc />
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.AddColumn<DateTime>(
+                name: "ModifiedOn",
+                table: "Notes",
+                type: "datetime2",
+                nullable: true);
+
+            migrationBuilder.AddColumn<int>(
+                name: "Status",
+                table: "Notes",
+                type: "int",
+                nullable: false,
+                defaultValue: 0);
+        }
+
+        /// <inheritdoc />
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.DropColumn(
+                name: "ModifiedOn",
+                table: "Notes");
+
+            migrationBuilder.DropColumn(
+                name: "Status",
+                table: "Notes");
+        }
+    }
+}
