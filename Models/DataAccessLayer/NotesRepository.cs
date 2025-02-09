23a5235c6eef0e59ed72a0e31287331601656aa5
Piyush Raj <piyushraj2340@gmail.com>
2025-02-10 00:42:33 +0530

    Add Notes Services Module and comment the un-used Notes repository

--- Models/DataAccessLayer/NotesRepository.cs
+++ Models/DataAccessLayer/NotesRepository.cs
@@ -18,35 +18,35 @@ namespace TaskMonitoringApp.Models.DataAccessLayer
             await _context.SaveChangesAsync();
         }
 
-        public async Task<IEnumerable<T>> GetAllNotesAsync<T>(string userId, ResponseDataMode mode) where T : class
-        {
-            return mode switch
-            {
-                ResponseDataMode.Model => await _context.Notes
-                    .Where(n => n.UserId == userId && n.IsDeleted == false)
-                    .ToListAsync() as IEnumerable<T>
-                        ?? throw new NotFoundException("Notes with userId Not Found!"),
-                ResponseDataMode.ModelDTO => await _context.Notes
-                    .Where(n => n.UserId == userId && n.IsDeleted == false)
-                    .Select(n => new NoteDTO()
-                    {
-                        Id = n.Id,
-                        Title = n.Title,
-                        Content = n.Content,
-                        Tags = n.Tags,
-                        IsModified = n.IsModified,
-                        ModifiedOn = n.ModifiedOn,
-                        ParentNoteId = n.ParentNoteId,
-                        Status = n.Status,
-                        IsPinned = n.IsPinned,
-                        TimeStamp = n.TimeStamp,
-                        UserId = n.UserId
-                    })
-                    .ToListAsync() as IEnumerable<T>
-                        ?? throw new NotFoundException("NoteDTO with userId Data Not Found!"),
-                _ => throw new InvalidOperationException("Invalid Operations While Fetching Notes Data.")
-            };
-        }
+        //public async Task<IEnumerable<T>> GetAllNotesAsync<T>(string userId, ResponseDataMode mode) where T : class
+        //{
+        //    return mode switch
+        //    {
+        //        ResponseDataMode.Model => await _context.Notes
+        //            .Where(n => n.UserId == userId && n.IsDeleted == false)
+        //            .ToListAsync() as IEnumerable<T>
+        //                ?? throw new NotFoundException("Notes with userId Not Found!"),
+        //        ResponseDataMode.ModelDTO => await _context.Notes
+        //            .Where(n => n.UserId == userId && n.IsDeleted == false)
+        //            .Select(n => new NoteDTO()
+        //            {
+        //                Id = n.Id,
+        //                Title = n.Title,
+        //                Content = n.Content,
+        //                Tags = n.Tags,
+        //                IsModified = n.IsModified,
+        //                ModifiedOn = n.ModifiedOn,
+        //                ParentNoteId = n.ParentNoteId,
+        //                Status = n.Status,
+        //                IsPinned = n.IsPinned,
+        //                TimeStamp = n.TimeStamp,
+        //                UserId = n.UserId
+        //            })
+        //            .ToListAsync() as IEnumerable<T>
+        //                ?? throw new NotFoundException("NoteDTO with userId Data Not Found!"),
+        //        _ => throw new InvalidOperationException("Invalid Operations While Fetching Notes Data.")
+        //    };
+        //}
 
         public async Task<IEnumerable<T>> GetAllNotesAsync<T>(string userId, Status status, ResponseDataMode mode) where T : class
         {
@@ -78,63 +78,63 @@ namespace TaskMonitoringApp.Models.DataAccessLayer
             };
         }
 
-        public async Task<IEnumerable<T>> GetAllNotesForBothGoalAndTaskIdAsync<T>(string userId, int goalId, int taskId, Status status, ResponseDataMode mode) where T : class
-        {
-            return mode switch
-            {
-                ResponseDataMode.Model => await _context.Notes
-                    .Where(n => n.UserId == userId && (status == Status.All || n.Status == status) && n.GoalId == goalId && n.TaskId == taskId && n.IsDeleted == false)
-                    .Include(g => g.Goal)
-                    .Include(t => t.Task)
-                    .AsSingleQuery()
-                    .ToListAsync() as IEnumerable<T>
-                        ?? throw new NotFoundException("Notes with userId Not Found!"),
-                ResponseDataMode.ModelDTO => await _context.Notes
-                    .Where(n => n.UserId == userId && (status == Status.All || n.Status == status) && n.GoalId == goalId && n.TaskId == taskId && n.IsDeleted == false)
-                    .Include(g => g.Goal)
-                    .Include(t => t.Task)
-                    .Select(n => new NoteDTOWithGoalAndTaskDTO()
-                    {
-                        Id = n.Id,
-                        Title = n.Title,
-                        Content = n.Content,
-                        Tags = n.Tags,
-                        IsModified = n.IsModified,
-                        ModifiedOn = n.ModifiedOn,
-                        ParentNoteId = n.ParentNoteId,
-                        Status = n.Status,
-                        IsPinned = n.IsPinned,
-                        TimeStamp = n.TimeStamp,
-                        UserId = n.UserId,
-                        Task = new TaskDTO()
-                        {
-                            Id = n.Task.Id,
-                            Name = n.Task.Name,
-                            Description = n.Task.Description,
-                            Repeat = n.Task.Repeat,
-                            RepeatWeekList = n.Task.RepeatWeekList,
-                            Priority = n.Task.Priority,
-                            EndDate = n.Task.EndDate,
-                            UserId = n.Task.UserId,
-                            TaskStatus = n.Task.TaskStatus
-                        },
-                        Goal = new GoalDTO()
-                        {
-                            Id = n.Goal.Id,
-                            Name = n.Goal.Name,
-                            Description = n.Goal.Description,
-                            Priority = n.Goal.Priority,
-                            EndDate = n.Goal.EndDate,
-                            UserId = n.Goal.UserId,
-                            GoalStatus = n.Goal.GoalStatus
-                        }
-                    })
-                    //.AsSingleQuery()
-                    .ToListAsync() as IEnumerable<T>
-                        ?? throw new NotFoundException("NoteDTOWithGoalAndTaskDTO with userId Data Not Found!"),
-                _ => throw new InvalidOperationException("Invalid Operations While Fetching Notes Data.")
-            };
-        }
+        //public async Task<IEnumerable<T>> GetAllNotesForBothGoalAndTaskIdAsync<T>(string userId, int goalId, int taskId, Status status, ResponseDataMode mode) where T : class
+        //{
+        //    return mode switch
+        //    {
+        //        ResponseDataMode.Model => await _context.Notes
+        //            .Where(n => n.UserId == userId && (status == Status.All || n.Status == status) && n.GoalId == goalId && n.TaskId == taskId && n.IsDeleted == false)
+        //            .Include(g => g.Goal)
+        //            .Include(t => t.Task)
+        //            .AsSingleQuery()
+        //            .ToListAsync() as IEnumerable<T>
+        //                ?? throw new NotFoundException("Notes with userId Not Found!"),
+        //        ResponseDataMode.ModelDTO => await _context.Notes
+        //            .Where(n => n.UserId == userId && (status == Status.All || n.Status == status) && n.GoalId == goalId && n.TaskId == taskId && n.IsDeleted == false)
+        //            .Include(g => g.Goal)
+        //            .Include(t => t.Task)
+        //            .Select(n => new NoteDTOWithGoalAndTaskDTO()
+        //            {
+        //                Id = n.Id,
+        //                Title = n.Title,
+        //                Content = n.Content,
+        //                Tags = n.Tags,
+        //                IsModified = n.IsModified,
+        //                ModifiedOn = n.ModifiedOn,
+        //                ParentNoteId = n.ParentNoteId,
+        //                Status = n.Status,
+        //                IsPinned = n.IsPinned,
+        //                TimeStamp = n.TimeStamp,
+        //                UserId = n.UserId,
+        //                Task = new TaskDTO()
+        //                {
+        //                    Id = n.Task.Id,
+        //                    Name = n.Task.Name,
+        //                    Description = n.Task.Description,
+        //                    Repeat = n.Task.Repeat,
+        //                    RepeatWeekList = n.Task.RepeatWeekList,
+        //                    Priority = n.Task.Priority,
+        //                    EndDate = n.Task.EndDate,
+        //                    UserId = n.Task.UserId,
+        //                    TaskStatus = n.Task.TaskStatus
+        //                },
+        //                Goal = new GoalDTO()
+        //                {
+        //                    Id = n.Goal.Id,
+        //                    Name = n.Goal.Name,
+        //                    Description = n.Goal.Description,
+        //                    Priority = n.Goal.Priority,
+        //                    EndDate = n.Goal.EndDate,
+        //                    UserId = n.Goal.UserId,
+        //                    GoalStatus = n.Goal.GoalStatus
+        //                }
+        //            })
+        //            //.AsSingleQuery()
+        //            .ToListAsync() as IEnumerable<T>
+        //                ?? throw new NotFoundException("NoteDTOWithGoalAndTaskDTO with userId Data Not Found!"),
+        //        _ => throw new InvalidOperationException("Invalid Operations While Fetching Notes Data.")
+        //    };
+        //}
 
         public async Task<IEnumerable<T>> GetAllNotesForGoalIdAsync<T>(string userId, int goalId, Status status, ResponseDataMode mode) where T : class
         {
@@ -267,74 +267,74 @@ namespace TaskMonitoringApp.Models.DataAccessLayer
             }
         }
 
-        public async Task<T> GetNotesByIdWithBothGoalAndTaskIdAsync<T>(string userId, int id, int goalId, int taskId, ResponseDataMode mode) where T : class
-        {
-            switch (mode)
-            {
-                case ResponseDataMode.Model:
-                    var dataModel = await _context.Notes
-                        .Where(n => n.UserId == userId && n.GoalId == goalId && n.TaskId == taskId && n.IsDeleted == false)
-                        .Include(g => g.Goal)
-                        .Include(t => t.Task)
-                        .AsSingleQuery()
-                        .ToListAsync() as IEnumerable<T>
-                            ?? throw new NotFoundException("Notes with userId Not Found!");
+        //public async Task<T> GetNotesByIdWithBothGoalAndTaskIdAsync<T>(string userId, int id, int goalId, int taskId, ResponseDataMode mode) where T : class
+        //{
+        //    switch (mode)
+        //    {
+        //        case ResponseDataMode.Model:
+        //            var dataModel = await _context.Notes
+        //                .Where(n => n.UserId == userId && n.GoalId == goalId && n.TaskId == taskId && n.IsDeleted == false)
+        //                .Include(g => g.Goal)
+        //                .Include(t => t.Task)
+        //                .AsSingleQuery()
+        //                .ToListAsync() as IEnumerable<T>
+        //                    ?? throw new NotFoundException("Notes with userId Not Found!");
 
-                    return dataModel.FirstOrDefault()
-                            ?? throw new NotFoundException("Notes with userId Not Found!");
+        //            return dataModel.FirstOrDefault()
+        //                    ?? throw new NotFoundException("Notes with userId Not Found!");
 
-                case ResponseDataMode.ModelDTO:
-                    var dataModelDTO = await _context.Notes
-                       .Where(n => n.UserId == userId && n.GoalId == goalId && n.TaskId == taskId && n.IsDeleted == false)
-                       .Include(g => g.Goal)
-                       .Include(t => t.Task)
-                       .Select(n => new NoteDTOWithGoalAndTaskDTO()
-                       {
-                           Id = n.Id,
-                           Title = n.Title,
-                           Content = n.Content,
-                           Tags = n.Tags,
-                           IsModified = n.IsModified,
-                           ModifiedOn = n.ModifiedOn,
-                           ParentNoteId = n.ParentNoteId,
-                           Status = n.Status,
-                           IsPinned = n.IsPinned,
-                           TimeStamp = n.TimeStamp,
-                           UserId = n.UserId,
-                           Task = new TaskDTO()
-                           {
-                               Id = n.Task.Id,
-                               Name = n.Task.Name,
-                               Description = n.Task.Description,
-                               Repeat = n.Task.Repeat,
-                               RepeatWeekList = n.Task.RepeatWeekList,
-                               Priority = n.Task.Priority,
-                               EndDate = n.Task.EndDate,
-                               UserId = n.Task.UserId,
-                               TaskStatus = n.Task.TaskStatus
-                           },
-                           Goal = new GoalDTO()
-                           {
-                               Id = n.Goal.Id,
-                               Name = n.Goal.Name,
-                               Description = n.Goal.Description,
-                               Priority = n.Goal.Priority,
-                               EndDate = n.Goal.EndDate,
-                               UserId = n.Goal.UserId,
-                               GoalStatus = n.Goal.GoalStatus
-                           }
-                       })
-                       .AsSingleQuery()
-                       .ToListAsync() as IEnumerable<T>
-                           ?? throw new NotFoundException("Notes with userId Not Found!");
+        //        case ResponseDataMode.ModelDTO:
+        //            var dataModelDTO = await _context.Notes
+        //               .Where(n => n.UserId == userId && n.GoalId == goalId && n.TaskId == taskId && n.IsDeleted == false)
+        //               .Include(g => g.Goal)
+        //               .Include(t => t.Task)
+        //               .Select(n => new NoteDTOWithGoalAndTaskDTO()
+        //               {
+        //                   Id = n.Id,
+        //                   Title = n.Title,
+        //                   Content = n.Content,
+        //                   Tags = n.Tags,
+        //                   IsModified = n.IsModified,
+        //                   ModifiedOn = n.ModifiedOn,
+        //                   ParentNoteId = n.ParentNoteId,
+        //                   Status = n.Status,
+        //                   IsPinned = n.IsPinned,
+        //                   TimeStamp = n.TimeStamp,
+        //                   UserId = n.UserId,
+        //                   Task = new TaskDTO()
+        //                   {
+        //                       Id = n.Task.Id,
+        //                       Name = n.Task.Name,
+        //                       Description = n.Task.Description,
+        //                       Repeat = n.Task.Repeat,
+        //                       RepeatWeekList = n.Task.RepeatWeekList,
+        //                       Priority = n.Task.Priority,
+        //                       EndDate = n.Task.EndDate,
+        //                       UserId = n.Task.UserId,
+        //                       TaskStatus = n.Task.TaskStatus
+        //                   },
+        //                   Goal = new GoalDTO()
+        //                   {
+        //                       Id = n.Goal.Id,
+        //                       Name = n.Goal.Name,
+        //                       Description = n.Goal.Description,
+        //                       Priority = n.Goal.Priority,
+        //                       EndDate = n.Goal.EndDate,
+        //                       UserId = n.Goal.UserId,
+        //                       GoalStatus = n.Goal.GoalStatus
+        //                   }
+        //               })
+        //               .AsSingleQuery()
+        //               .ToListAsync() as IEnumerable<T>
+        //                   ?? throw new NotFoundException("Notes with userId Not Found!");
 
-                    return dataModelDTO.FirstOrDefault()
-                            ?? throw new NotFoundException("Notes with userId Not Found!");
+        //            return dataModelDTO.FirstOrDefault()
+        //                    ?? throw new NotFoundException("Notes with userId Not Found!");
 
-                default:
-                    throw new InvalidOperationException("Invalid Operations While Fetching Notes Data.");
-            }
-        }
+        //        default:
+        //            throw new InvalidOperationException("Invalid Operations While Fetching Notes Data.");
+        //    }
+        //}
 
         public async Task<T> GetNotesByIdWithGoalIdAsync<T>(string userId, int id, int goalId, ResponseDataMode mode) where T : class
         {
---
+++ Models/Services/INotesServices.cs
@@ -0,0 +1,31 @@
+﻿using TaskMonitoringApp.Models.DTOs;
+using TaskMonitoringApp.Models.Entities;
+
+namespace TaskMonitoringApp.Models.Services
+{
+    public interface INotesServices
+    {
+        Task<IEnumerable<Notes>> GetAllNotesWithFullContext(string userId, Status status);
+
+        Task<IEnumerable<NoteDTO>> GetAllNotes(string userId, Status status);
+
+        Task<IEnumerable<NoteDTOWithGoalDTO>> GetAllNotesByGoalId(string userId, int goalId, Status status);
+        
+        Task<IEnumerable<NoteDTOWithTaskDTO>> GetAllNotesByTaskId(string userId, int taskId, Status status);
+
+        Task<Notes> GetNoteByIdWithFullContext(string userId, int notesId, Status status);
+
+        Task<NoteDTO> GetNoteById(string userId, int notesId,  Status status);
+
+        Task<NoteDTOWithGoalDTO> GetNoteByIdByGoalId(string userId, int notesId,  int goalId, Status status);
+
+        Task<NoteDTOWithTaskDTO> GetNoteByIdByTaskId(string userId, int notesId,  int taskId, Status status);
+
+        Task AddNotesWithGoalId(string userId, NoteDTO notes, int goalId);
+
+        Task AddNotesWithTaskId(string userId, NoteDTO notes, int taskId);
+
+        Task UpdateNotes(string userId, NoteDTO noteToUpdate);
+
+    }
+}
