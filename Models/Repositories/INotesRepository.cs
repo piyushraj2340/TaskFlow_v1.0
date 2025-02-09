6e17f0f3be5d42358257e62603c3db529a2ad32e
Piyush Raj <piyushraj2340@gmail.com>
2025-02-10 00:42:33 +0530

    Add Notes Services Module and comment the un-used Notes repository

--- Models/Repositories/INotesRepository.cs
+++ Models/Repositories/INotesRepository.cs
@@ -4,7 +4,7 @@ namespace TaskMonitoringApp.Models.Repositories
 {
     public interface INotesRepository
     {
-        Task<IEnumerable<T>> GetAllNotesAsync<T>(string userId, ResponseDataMode mode) where T: class;
+        //Task<IEnumerable<T>> GetAllNotesAsync<T>(string userId, ResponseDataMode mode) where T: class;
 
         Task<IEnumerable<T>> GetAllNotesAsync<T>(string userId, Status status, ResponseDataMode mode) where T : class;
 
@@ -12,7 +12,7 @@ namespace TaskMonitoringApp.Models.Repositories
 
         Task<IEnumerable<T>> GetAllNotesForTaskIdAsync<T>(string userId, int taskId, Status status, ResponseDataMode mode) where T : class;
 
-        Task<IEnumerable<T>> GetAllNotesForBothGoalAndTaskIdAsync<T>(string userId, int goalId, int taskId, Status status, ResponseDataMode mode) where T : class;
+        //Task<IEnumerable<T>> GetAllNotesForBothGoalAndTaskIdAsync<T>(string userId, int goalId, int taskId, Status status, ResponseDataMode mode) where T : class;
 
         Task<T> GetNotesByIdAsync<T>(string userId, int id, ResponseDataMode mode) where T : class;
 
@@ -20,7 +20,7 @@ namespace TaskMonitoringApp.Models.Repositories
 
         Task<T> GetNotesByIdWithTaskIdAsync<T>(string userId, int id, int taskId, ResponseDataMode mode)where T : class;
 
-        Task<T> GetNotesByIdWithBothGoalAndTaskIdAsync<T>(string userId, int id, int goalId, int taskId, ResponseDataMode mode)where T : class;
+        //Task<T> GetNotesByIdWithBothGoalAndTaskIdAsync<T>(string userId, int id, int goalId, int taskId, ResponseDataMode mode)where T : class;
 
         Task UpdateNotes(string userId, Notes notes);
 
