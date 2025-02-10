2c46fc5d0639f14ee15950f7ad815c878d66c70e
Piyush Raj <piyushraj2340@gmail.com>
2025-02-11 01:45:36 +0530

    Add Create Notes Action in NotesController and fix UI and handel ajax to manage API

--- Controllers/NotesController.cs
+++ Controllers/NotesController.cs
@@ -1,17 +1,43 @@
-﻿using Microsoft.AspNetCore.Mvc;
+﻿using Microsoft.AspNetCore.Authorization;
+using Microsoft.AspNetCore.Identity;
+using Microsoft.AspNetCore.Mvc;
+using TaskMonitoringApp.Models.DTOs;
+using TaskMonitoringApp.Models.Entities;
+using TaskMonitoringApp.Models.Services;
 
 namespace TaskMonitoringApp.Controllers
 {
-    public class NotesController : Controller
+    [Authorize]
+    public class NotesController(INotesServices service, UserManager<Users> userManager) : Controller
     {
+        private readonly INotesServices _service = service;
+        private readonly UserManager<Users> _userManager = userManager;
         public IActionResult Index()
         {
             return View();
         }
 
-        public IActionResult Create()
+        [HttpPost]
+        public async Task<IActionResult> Create(int Id, [Bind("Title,Content,Tags")]NoteDTO note)
         {
-            return View();
+            // Get the logged-in user's ID
+            var userId = _userManager.GetUserId(User);
+
+            if (userId == null)
+            {
+                return RedirectToAction("Login", "Account");
+            }
+
+            if (ModelState.IsValid)
+            {
+                note.UserId = userId;
+                await _service.AddNotesWithGoalId(userId, note, Id);
+
+                return Json(new { status = true, message = "Your Notes Saved!" });
+            }
+
+            return Json(new { status = false, message = "Your data does't save!" });
+
         }
     }
 }
